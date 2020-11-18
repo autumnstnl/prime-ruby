4ca1e71dab008dcecc1d08469c3014f1aea99400
Autumn <autumnstnl@gmail.com>
2020-11-18 17:45:21 -0500

    commit tree 2147fc58cdf908b27b3c77180ff528841f7a3648

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
 # Add  code here!
+def prime?(number)
+    if number > 1
+    array = (2..(number-1)).to_a 
+    array.none? do |num| 
+        number % num == 0
+    end
+    else
+        return false
+        end
+    end
+
+
+
+
+
+    
